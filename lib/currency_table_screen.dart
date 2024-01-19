2e7d9f20cfb955772c5349bf6319e201ca593ef1
MoustaphaCamara <cmr.mous@gmail.com>
2024-01-19 14:41:13 +0100

    expanded test

--- lib/currency_table_screen.dart
+++ lib/currency_table_screen.dart
@@ -3,9 +3,25 @@ import 'package:flutter/material.dart';
 class CurrencyTableScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-          child: Text('salut'))
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Currencies converter')
+      ),
+        body: Column(
+          children: [
+            Expanded(
+              flex:1,
+              child: Container(
+                width: double.infinity,
+                color: Colors.red,
+                child: Text("dzhofhzeufo"),
+              )
+            ),
+            const Text("hi", style: TextStyle(fontSize: 18, color: Colors.blue)),
+            const Text("second textferznof", style: TextStyle(fontSize: 18, color: Colors.blue)),
+            const Text("second"),
+          ],
+        )
         );
   }
 }
