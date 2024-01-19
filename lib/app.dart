b8afe1aa7819ddca31e016ac0b2f99379c2fb1d4
MoustaphaCamara <cmr.mous@gmail.com>
2024-01-19 14:29:49 +0100

    Batman - commit has no parents

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:currency/currency_table_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: CurrencyTableScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:currency/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
